6b5a0de870d9c65400938534c19c416ab12bd5e4
parkjieun <p_n49@naver.com>
2025-03-07 12:28:24 +0900

    250307 work 반응형 완

--- js/index_gsap.js
+++ js/index_gsap.js
@@ -1,27 +1,57 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
 
-  gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: ".sec1",
-        start: "0% 0%",
-        end: "20% 5%",
-        scrub: 2, // 부드럽게
-        // markers: true, // 확인용
-      },
-    })
-    .fromTo(
-      ".sec1 .con",
-      {
-        width: "1600px",
-        height: "600px",
-      },
-      {
-        width: "100vw", // 화면을 가득 채우도록
-        height: "100vh",
-        borderRadius: "0px", // 모서리 없애기
-        ease: "power2.out",
-      }
-    );
+  let mm = gsap.matchMedia();
+
+  mm.add("(min-width: 1600px)", () => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".sec1",
+          start: "0% 0%",
+          end: "20% 5%",
+          scrub: 1, // 부드럽게
+          // markers: true, // 확인용
+        },
+      })
+      .fromTo(
+        ".sec1 .con",
+        {
+          width: "1600px",
+          height: "600px",
+        },
+        {
+          width: "100vw", // 화면을 가득 채우도록
+          height: "100vh",
+          borderRadius: "0px", // 모서리 없애기
+          ease: "power2.out",
+        }
+      );
+  });
+
+  mm.add("(max-width: 1599px)", () => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".sec1",
+          start: "0% 0%",
+          end: "20% 5%",
+          scrub: 1, // 부드럽게
+          // markers: true, // 확인용
+        },
+      })
+      .fromTo(
+        ".sec1 .con",
+        {
+          width: "90%",
+          height: "500px",
+        },
+        {
+          width: "100vw", // 화면을 가득 채우도록
+          height: "100vh",
+          borderRadius: "0px", // 모서리 없애기
+          ease: "power2.out",
+        }
+      );
+  });
 });
